9faa1736372ac246691cc2a4da64a76a858b920d
Denali Balser <denali.balser167@gmail.com>
2019-05-15 20:52:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
 def oxford_comma(array)
-  if array.length = 2
-     array.join(" and ")
-  elsif array.length < 2
-    array.join(" , ").push("and", -1)
+  if array.length >= 3
+    array.last.insert(0, "and ")
+    #array.insert(-2, "and")
+  elsif array.length == 2
+    array.insert(1, " and ")
+    return array.join
   else
-    array.join
-  end
+    array
+ end
+ array.join(", ")
 end
